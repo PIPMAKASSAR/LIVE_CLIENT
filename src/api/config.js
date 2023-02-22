8a8c780d10e94a0220fbf7fdb8b91933af236b64
mustaqinIshak <mustaqinishak@gmail.com>
2023-02-22 20:19:51 +0800

    change api

--- src/api/config.js
+++ src/api/config.js
@@ -1,13 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-<<<<<<< HEAD
-    baseURL: "https://apinavicmapus.pipmakassar.ac.id/api/live"
-    // baseURL: "https://genius-im.com/api/live"
-=======
-    // baseURL: "https://apinavicmapus.pipmakassar.ac.id/api/live"
-    baseURL: "https://genius-im.com/api/live"
->>>>>>> 6f58f42e474e6c4382e246ed2f2857adc84cae94
+    baseURL: "https://apinavicampus.pipmakassar.ac.id/api/live"
+   
 })
 
 export default instance
