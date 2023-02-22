6f58f42e474e6c4382e246ed2f2857adc84cae94
mustaqinIshak <55459373+mustaqinIshak@users.noreply.github.com>
2023-02-22 16:31:12 +0800

    api genius

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://apinavicmapus.pipmakassar.ac.id/api/live"
+    // baseURL: "https://apinavicmapus.pipmakassar.ac.id/api/live"
+    baseURL: "https://genius-im.com/api/live"
 })
 
 export default instance
